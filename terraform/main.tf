f6346acfc57a0afe7b7404c3ab1d6953dc564c6f
Taha Çakır <77748816+Taha-Cakir@users.noreply.github.com>
2023-10-11 21:24:20 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,25 @@ resource "kubeflowpipelines_pipeline" "mlpipeline_coupon" {
 }
 
 
+# Create a Kubeflow Pipelines job to trigger the Kubeflow pipeline
+resource "kubeflowpipelines_job" "pipeline_trigger" {
+    name            = "pipeline-trigger"
+    description     = "Trigger for coupon Kubeflow pipeline"
+    service_account = "pipeline-runner"
+    enabled         = true
+    max_concurrency = 2
+    no_catchup      = true
+
+    pipeline_spec {
+        pipeline_version_id = kubeflowpipelines_pipeline.mlpipeline_coupon.version_id
+    }
+
+    trigger {
+        cron_schedule {
+            start_time = "2023-06-23T00:00:00Z"
+            end_time   = "2024-06-23T00:00:00Z"
+            cron       = "0 0 1 * * ?"
+        }
+    }
+}
+
