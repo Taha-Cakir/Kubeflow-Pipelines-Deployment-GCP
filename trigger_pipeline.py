4c8ba9d7c95afbf52698510ad5f9a658ebc71d8b
Taha Çakır <77748816+Taha-Cakir@users.noreply.github.com>
2023-10-11 21:26:57 +0300

    Update trigger_pipeline.py

--- trigger_pipeline.py
+++ trigger_pipeline.py
@@ -1,12 +1,12 @@
 from google.cloud import aiplatform
 
-PROJECT_ID = "udemy-mlops"
+PROJECT_ID = "cakir-kubeflow"
 REGION = "us-central1"
 aiplatform.init(project=PROJECT_ID,location=REGION)
 
 job = aiplatform.PipelineJob(
     display_name='trigger-coupon-model-pipeline',
-    template_path="gs://mlops-cakir-kubeflow-v1/compiled_pipelines/coupon-pipeline.json",
+    template_path="gs://mlops-cakir-kubeflow-v1/mlops-recommendation/mlops-recommendation-pipeline-deploy-v1.json",
     pipeline_root="gs://mlops-cakir-kubeflow-v1/coupon-pipeline-v1",
     enable_caching=False
 )
